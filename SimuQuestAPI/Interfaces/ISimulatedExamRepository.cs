cd2a26a1af7e0ba96344829bdf73e331c4d91d00
Clayton Rocha <clayton.will08@gmail.com>
2025-08-25 01:52:04 -0300

    Criação das Interfaces

---
+++ SimuQuestAPI/Interfaces/ISimulatedExamRepository.cs
@@ -0,0 +1,13 @@
+﻿using SimuQuestAPI.Models;
+
+namespace SimuQuestAPI.Interfaces
+{
+    public interface ISimulatedExamRepository
+    {
+        Task Add(SimulatedExam exam);
+        Task<IEnumerable<SimulatedExam>> GetAll();
+        Task<SimulatedExam> GetById(int id);
+        Task Update(SimulatedExam exam);
+        Task Delete(int id);
+    }
+}
